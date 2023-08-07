b33831168fb1202846c06f6d5a9e114c516d8b49
Casper <casper.vissers@rws.nl>
2023-08-07 14:21:34 +0200

    Style Sheet class

---
+++ Assets/Scripts/UI/StyleSheets.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace UI
+{
+    /// <summary>
+    /// Names of the used style sheets and selectors within the project
+    /// </summary>
+    public static class StyleSheets 
+    {
+        public static class General
+        {
+            public const string styleSheet = "general";
+
+            public const string textFont = "text-font";
+            public const string textSize1 = "text-size1";
+            public const string textSize2 = "text-size2";
+            public const string textSize3 = "text-size3";
+
+            public static StyleSheet GetStyleSheet() => StyleSheets.GetStyleSheet(styleSheet);
+        }
+
+        public static class Bar
+        {
+            public const string styleSheet = "bar";
+
+            public const string container = "bar-container";
+            public const string bar = "bar";
+            public const string textTop = "bar-text-top";
+            public const string textBottom = "bar-text-bottom";
+            public const string dashed = "dashed";
+
+            public static StyleSheet GetStyleSheet() => StyleSheets.GetStyleSheet(styleSheet);
+        }
+
+        public static StyleSheet GetStyleSheet(string styleSheetName)
+        {
+            return Resources.Load<StyleSheet>(styleSheetName);
+
+        }
+    }
+}
