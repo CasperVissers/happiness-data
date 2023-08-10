9662746bd243ececdd174e610c320551961f3bbf
Casper <casper.vissers@rws.nl>
2023-08-10 08:01:24 +0200

    Use Resources.Load to get data

--- Assets/Scripts/Happiness/HappinessDataGetter.cs
+++ Assets/Scripts/Happiness/HappinessDataGetter.cs
@@ -1,19 +1,22 @@
 using Newtonsoft.Json;
 using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
 
 namespace Happiness
 {
     public static class HappinessDataGetter
     {
-        const string FILE_NAME = "Assets/Resources/Happiness Data/happinessData.json";
+        const string FOLDER = "Happiness Data";
+        const string FILE_NAME = "happinessData";
 
         /// <summary>
         /// Deserializes a json file into a list of happiness data.
         /// </summary>
         public static List<HappinessData> GetHappinessData()
         {
-            var jsonFile = System.IO.File.ReadAllText(FILE_NAME);
-            return JsonConvert.DeserializeObject<List<HappinessData>>(jsonFile);
+            var jsonFile = Resources.Load<TextAsset>($"{FOLDER}/{FILE_NAME}");
+            return JsonConvert.DeserializeObject<List<HappinessData>>(jsonFile.text);
         }
     }
 }
